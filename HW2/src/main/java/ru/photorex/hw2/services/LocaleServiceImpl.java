05065402ded1dfb809425d600200fe0b9a637fd6
Dima Merechko <mfocus@mail.ru>
2019-09-05 15:58:32 +0200

    initial locale from user system

--- HW2/src/main/java/ru/photorex/hw2/services/LocaleServiceImpl.java
+++ HW2/src/main/java/ru/photorex/hw2/services/LocaleServiceImpl.java
@@ -1,18 +1,25 @@
 package ru.photorex.hw2.services;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
 
-@RequiredArgsConstructor
 @Service
 public class LocaleServiceImpl implements LocaleService {
 
     private final MessageSource ms;
     private final IOService consoleService;
-    private Locale locale = Locale.ENGLISH;
+    private Locale locale;
+
+    public LocaleServiceImpl(MessageSource ms,
+                             IOService consoleService,
+                             @Value("#{ systemProperties['user.region'] }") Locale locale) {
+        this.ms = ms;
+        this.consoleService = consoleService;
+        this.locale = locale;
+    }
 
     @Override
     public Locale selectUserLocaleAndFileForPoll() {
