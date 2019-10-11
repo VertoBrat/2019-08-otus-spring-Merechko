2529f3d34ddff4f0c733eeecc181cca9ae29f81a
Dima Merechko <mfocus@mail.ru>
2019-10-11 12:14:27 +0200

    init commit

---
+++ HW8/src/main/java/ru/photorex/hw8/LibraryApplication.java
@@ -0,0 +1,15 @@
+package ru.photorex.hw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
