ac50eeeb7ad7bad0cc4e6dae379c1907c18fa03a
Dima Merechko <mfocus@mail.ru>
2019-10-25 12:31:33 +0200

    add mongock dbFilling

--- HW10/server/src/main/java/ru/photorex/server/LibraryServer.java
+++ HW10/server/src/main/java/ru/photorex/server/LibraryServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryApplication.class, args);
+        SpringApplication.run(LibraryServer.class, args);
     }
 
 }
