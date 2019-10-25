a0fe7cea96d6870e8c6b58d7343eff515dd4e00c
Dima Merechko <mfocus@mail.ru>
2019-10-25 09:19:26 +0200

    init server

---
+++ HW10/server/src/main/java/ru/photorex/server/LibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.photorex.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
