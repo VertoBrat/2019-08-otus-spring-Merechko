fe09849455f731201c3540ea231e055adbde091a
Dima Merechko <mfocus@mail.ru>
2019-12-07 22:20:40 +0200

    add services

---
+++ HW15/src/main/java/ru/photorex/hw15/service/IOService.java
@@ -0,0 +1,8 @@
+package ru.photorex.hw15.service;
+
+public interface IOService {
+
+    void printString(String s);
+
+    void printFormattedString(String s, Object...objects);
+}
---
+++ HW15/src/main/java/ru/photorex/hw15/service/KitchenService.java
@@ -0,0 +1,33 @@
+package ru.photorex.hw15.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.photorex.hw15.model.Drink;
+import ru.photorex.hw15.model.Food;
+import ru.photorex.hw15.model.FoodItem;
+
+import java.util.concurrent.TimeUnit;
+
+import static ru.photorex.hw15.IntegrationConfig.sleepWithoutTryCatch;
+
+@Service
+@RequiredArgsConstructor
+public class KitchenService {
+
+    private final IOService ioService;
+
+    public Food cookHotFoodItem(FoodItem item) {
+        return foodMaker(3, TimeUnit.SECONDS, item);
+    }
+
+    public Food cookColdFoodItem(FoodItem item) {
+        return foodMaker(1, TimeUnit.SECONDS, item);
+    }
+
+    private Food foodMaker(int sleep, TimeUnit unit, FoodItem item) {
+        ioService.printFormattedString("Cooking %s", item.getItemName());
+        sleepWithoutTryCatch(sleep, unit);
+        ioService.printFormattedString("Cooking %s DONE", item.getItemName());
+        return new Food(item.getOrderId(), item.getItemName());
+    }
+}
